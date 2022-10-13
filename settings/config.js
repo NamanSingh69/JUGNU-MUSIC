3ea824613e9731c80c9720bda25d9c521679b8f2
NamanSingh69 <114336969+NamanSingh69@users.noreply.github.com>
2022-10-13 17:58:55 +0530

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   TOKEN:
-    process.env.TOKEN ||
-    "",
+    client.login(MTAyOTgwNjIzODkyNzY5NTk2Mg.GPd7YN.qfXlwpqteuaXMNGszFr8w3URunxA8xVatz32BI),
   PREFIX: process.env.PREFIX || ".",
   embed: {
     color: process.env.color || "#220f80",
